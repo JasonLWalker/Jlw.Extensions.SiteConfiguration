9d91c15b984b4a92a9c5eb34eb7a02a50bebb09c
Jason Walker <jlwalker@spsmail.org>
2021-10-14 10:26:57 -0500

    Add test project and build scripts

---
+++ Jlw.Extensions.SiteConfiguration.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jlw.Extensions.SiteConfiguration.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
